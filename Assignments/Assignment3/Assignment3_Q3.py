a20593486b98c09cb617d79ef0bd57a5802cf3f2
xl3139 <xl3139@nyu.edu>
2021-11-23 10:51:50 -0500

    End of Assignment3_Q3.py

--- Assignments/Assignment3/Assignment3_Q3.py
+++ Assignments/Assignment3/Assignment3_Q3.py
@@ -19,7 +19,7 @@ plt.imshow(movie['X'][:, 0].reshape((160, 130)).swapaxes(0, 1), cmap='gray')
 plt.show()
 
 
-def robustPCA(X, delta=1e-6, mu=None, maxiter=10):
+def robustPCA(X, delta=2e-8, mu=None, maxiter=500):
 	"""
 	The function should return a PCA like part stored in 'L' with only a few singular values
 	that are non zero and a sparse sequence 'S' in which the images are black except w very
@@ -44,7 +44,7 @@ def robustPCA(X, delta=1e-6, mu=None, maxiter=10):
 
 		# Step 1. Compute and truncate the SVD
 		step1_temp = X - S - (1/mu) * Y
-		u, sigma_arrary, vt = fbpca.pca(step1_temp, raw=True)
+		u, sigma_arrary, vt = fbpca.pca(step1_temp, raw=True, n_iter=5)
 		truncated_sigma = np.sign(sigma_arrary) * np.maximum(np.abs(sigma_arrary)-mu, 0)
 		truncated_sigma_matrix = np.zeros((u.shape[1], vt.shape[0]))
 		for j in range(len(truncated_sigma)):
@@ -53,7 +53,7 @@ def robustPCA(X, delta=1e-6, mu=None, maxiter=10):
 
 		# Step 2. Truncate the entries of X - L + mu^(-1)Y
 		step2_temp = X - L + (1/mu) * Y
-		S = np.sign(step2_temp) * np.maximum(step2_temp - lam*mu, 0)
+		S = np.sign(step2_temp) * np.maximum(np.abs(step2_temp) - lam*mu, 0)
 
 		# Step 3. Update the matrix Y
 		Y = Y + mu * (X - L - S)
@@ -61,7 +61,7 @@ def robustPCA(X, delta=1e-6, mu=None, maxiter=10):
 		# Convergence criterion
 		err = np.sqrt(np.sum((X-L-S) ** 2) / norm)
 
-		print(np.sum((X-L-S)**2))
+		print(err)
 		if err < delta:
 			break
 		i += 1
@@ -70,5 +70,6 @@ def robustPCA(X, delta=1e-6, mu=None, maxiter=10):
 
 
 L_result, S_result = robustPCA(movie["X"])
-plt.imshow(L_result[:, 0].reshape((160, 130)).swapaxes(0, 1), cmap='gray')
+temp_image = np.hstack((L_result[:, 5].reshape(160, 130), S_result[:, 5].reshape(160, 130)))
+plt.imshow(temp_image.swapaxes(0, 1), cmap='gray')
 plt.show()
