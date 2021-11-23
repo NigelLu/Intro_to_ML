ce8d2481a44d6252dc140178d18bbf9d4c8400cf
xl3139 <xl3139@nyu.edu>
2021-11-23 13:37:04 -0500

    Modify Assignment3_Q1.py

--- Assignments/Assignment3/Assignment3_Q1.py
+++ Assignments/Assignment3/Assignment3_Q1.py
@@ -3,10 +3,12 @@
 from tensorflow.keras.layers import Conv2D
 
 from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.layers import MaxPooling2D
 import matplotlib.image as mpimg
+import tensorflow as tf
 import cv2
 
 img1 = mpimg.imread('1/00001_00000_00012.png')
@@ -35,7 +37,32 @@ img4 = cv2.resize(img4, (75, 75))
 
 model = Sequential()
 
-# model.add(Conv2D(num_units, (filter_size1, filter_size2), padding='same',
-#                              input_shape=(3, IMG_SIZE, IMG_SIZE),
-#                              activation='relu'))
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=(75, 75, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)), strides=None)
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)), strides=None)
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+print(model.summary())
 
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+print(model.summary())
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10,
+                    validation_data=(test_images, test_labels))
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print(test_acc)
